b7ceeb2f40447afa5ec1de699d249e6df2fefe78
Chandrabhan <chandrabhan.s.2002@gmail.com>
2022-11-15 17:44:03 +0530

    Layout created

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route>
+          <div>Error 404: Page not found</div>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
